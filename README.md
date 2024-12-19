78052e2ee073a499b02f3679663f6d8ed54cc8da
msueros <m.sueros.t97@gmail.com>
2024-12-18 22:43:52 -0500

    Agregado todos los archivos para el despliegue del proyecto

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sprint7_Project
 Data Analytics bootcamp - Sprint 7 Project
+El proyecto consta de crear una aplicación web que tenga un panel de control. Abarca desde la creación hasta su despliegue en un servicio en la nube.
+La aplicación muestra gráficos del análisis de la distribución del archivo vehicles que es un conjunto de datos de anuncios de venta de vehículos.
---
+++ app.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('C:/Users/user/sprint7_Project/vehicles_us.csv')
+
+st.header('Análisis de la Distribución de Vehicles')
+hist_button = st.button('Construir histograma') # crear un botón
+scat_button = st.button('Construir Gráfico de Dispersión')
+
+if hist_button: # al hacer clic en el botón
+    
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if scat_button:
+    # escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    
+    # crear un gráfico de dispersión
+    graph = px.scatter(car_data, x='odometer',y='price',title='Gráfico de Dispersión')
+
+    # mostrar el gráfico
+    st.plotly_chart(graph, use_container_width=True)
